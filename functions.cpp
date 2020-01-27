41bd9d77e39bfb06e083dc0176159e9c0520fc8a
wenxuan27 <wenxuan27@outlook.com>
2020-01-27 13:07:20 -0500

    finished display board

--- functions.cpp
+++ functions.cpp
@@ -6,19 +6,26 @@
 using namespace std;
 
 ​void greetAndInstruct(){
-
+  cout << "Hello and welcome to the Tic-Tac-Toe challenge: Player against Computer.\n" << endl;
+  cout << "The board is numbered from 1 to 27 as per the following:\n\n" << endl;
+  cout << "1 | 2 | 3" << "          " << "10 | 11 | 12" << "          " << "19 | 20 | 21" << endl;
+  cout << "---------" << "          " << "------------" << "          " << "------------" << endl;
+  cout << "4 | 5 | 6" << "          " << "13 | 14 | 15" << "          " << "22 | 23 | 24" << endl;
+  cout << "---------" << "          " << "------------" << "          " << "------------" << endl;
+  cout << "7 | 8 | 9" << "          " << "16 | 17 | 18" << "          " << "25 | 26 | 27\n" << endl;
+  cout << "Player starts first. Simply input the number of the cell you want to occupy. Player's move is marked with X. Computer's move is marked with O.\n" << endl;
+  cout << "Start? (y/n):" << endl;
 }
 
 void displayBoard(char board[]){
-  cout << "Hello and welcome to the Tic - Tac - Toe challenge : Player against Computer.\n
-  The board is numbered from 1 to 27 as per the following : \n\n
-  1 | 2 | 3              10 | 11 | 12               19 | 20 | 21\n
-  ---------              ------------               ------------
-  4 | 5 | 6              13 | 14 | 15               22 | 23 | 24\n
-  ---------              ------------               ------------
-  7 | 8 | 9              16 | 17 | 18               25 | 26 | 27\n\n
-  Player starts first. Simply input the number of the cell you want to occupy. Player’smove is marked with X. Computer’s move is marked with O.\n\n
-  Start ? (y / n) :" << endl;
+
+  cout << "1 | 2 | 3" << "          " << "10 | 11 | 12" << "          " << "19 | 20 | 21" << endl;
+  cout << "---------" << "          " << "------------" << "          " << "------------" << endl;
+  cout << "4 | 5 | 6" << "          " << "13 | 14 | 15" << "          " << "22 | 23 | 24" << endl;
+  cout << "---------" << "          " << "------------" << "          " << "------------" << endl;
+  cout << "7 | 8 | 9" << "          " << "16 | 17 | 18" << "          " << "25 | 26 | 27\n" << endl;
+
+  
 }
 
 bool checkIfLegal​(int cellNbre, char board[]){
--- main.cpp
+++ main.cpp
@@ -1,12 +1,41 @@
 #include <cmath>
 #include <cstdio>
+#include <array>
 // #include <vector>
 #include <iostream>
+
 // #include <algorithm>
 // #include "functions.cpp"
 using namespace std;
 
+void displayBoard(char board[])
+{
+  int n = strlen(board);
+  //    size_t n = sizeof(board)/sizeof(board[0]);
+  //    printf("%d = %d/%d\n", n, sizeof(board), sizeof(board[0]));
+  //    for(int i = 0; i < n; i++){
+  //        printf("%c", (board[i]));
+  //        cout << board[i];
+  //    }
+
+  for (int j = 0; j < 3; j++)
+  {
+    int t = 3 * j;
+    cout << board[t] << " | " << board[t + 1] << " | " << board[t + 2] << "          " << board[t + 9] << " | " << board[t + 10] << " | " << board[t + 11] << "          " << board[t + 18] << " | " << board[t + 19] << " | " << board[t + 20] << endl;
+    if (j < 2)
+    {
+      cout << "---------"
+           << "          "
+           << "---------"
+           << "          "
+           << "---------" << endl;
+    }
+  }
+}
+
 int main(void)
 {
-  cout << "helloworld" << endl;
+  char board[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '\0'};
+  printf("the length of board is %d\n", strlen(board));
+  displayBoard(board);
 }
